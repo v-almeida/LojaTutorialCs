bb0a9add70f7dd5df0958b3a190cc0e518097111
Vítor <almeida.vitor20000@gmail.com>
2024-06-30 02:37:33 -0300

    step 5 and 6 done.

--- obj/Debug/net8.0/Loja.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loja.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+866faf7955dc728d9b72bd9c42c9a0f16721cafd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54779297248cc9330e01006b9e3cc0d01d93e973")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
